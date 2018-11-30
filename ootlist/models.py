8d0e6c2efaaf8e830084970aafeb2cd69d5535be
Marc L <marcll@vt.edu>
2018-11-30 14:07:32 -0500

    CharField to TextField
    getting errors about strings being too long, trying TextField instead

--- ootlist/models.py
+++ ootlist/models.py
@@ -6,17 +6,17 @@ from django.db import models
 class Outoftreemodule(models.Model):
     name = models.CharField(max_length=500)
     tags = models.CharField(max_length=500, null=True, blank=True)
-    description = models.CharField(max_length=500, null=True, blank=True)
+    description = models.TextField(max_length=5000, null=True, blank=True)
     repo = models.URLField(max_length=500, null=True, blank=True)
     added_date = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=30, null=True, blank=True) # green, yellow, red
     last_commit = models.DateField(null=True, blank=True)
     author = models.CharField(max_length=500, null=True, blank=True)
-    dependencies = models.CharField(max_length=500, null=True, blank=True)
+    dependencies = models.TextField(max_length=5000, null=True, blank=True)
     copyright_owner = models.CharField(max_length=500, null=True, blank=True)
     icon = models.CharField(max_length=500, null=True, blank=True) # most urls provided by people dont even work anymore, so dont display the icon for now
     website = models.CharField(max_length=500, null=True, blank=True)
-    body_text = models.CharField(max_length=50000, null=True, blank=True)
+    body_text = models.TextField(max_length=50000, null=True, blank=True)
     
 class Packageversion(models.Model):
     os_name = models.CharField(max_length=500)
