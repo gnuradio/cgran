a3d152b86573e5b37db450c581dee1a9f532367b
Nicolas Cuervo <cuervonicolas@gmail.com>
2020-01-29 16:46:36 +0100

    Formatting: Remove whitespaces

--- ootlist/views.py
+++ ootlist/views.py
@@ -19,7 +19,7 @@ def index(request):
         form = SearchForm(request.POST)
         if form.is_valid(): # check whether it's valid
             # search for oots with the provided string in either the name, tags, description, authors, or body_text
-            objects = (Outoftreemodule.objects.filter(name__icontains=form.cleaned_data['search_text']) | 
+            objects = (Outoftreemodule.objects.filter(name__icontains=form.cleaned_data['search_text']) |
                        Outoftreemodule.objects.filter(tags__icontains=form.cleaned_data['search_text']) |
                        Outoftreemodule.objects.filter(description__icontains=form.cleaned_data['search_text']) |
                        Outoftreemodule.objects.filter(author__icontains=form.cleaned_data['search_text']) |
@@ -50,9 +50,9 @@ def refresh(request):
     import yaml # yaml parser
     from io import StringIO # allows strings to be converted to file object type things
     import re
-    import datetime 
+    import datetime
     import mistune # markdown parser
-    
+
     # get around unicode error because of symbols in peoples names
     #import sys
     #reload(sys)
@@ -106,33 +106,33 @@ def refresh(request):
                         giturl = giturl.replace('.git','') # remove the .git that most of them have at the end
                         giturl = giturl.replace('/releases/download/v2.6.1/protobuf-2.6.1.tar.gz','') # the protobufs recipe is a weird one
                         try: # a good chunk of OOTs dont have this file
-                            f = urllib.request.urlopen('https://raw.githubusercontent.com/' + giturl + '/master/MANIFEST.md') 
-                            manifest = f.read().decode('utf-8') # this converts it from a byte object to a python string   
+                            f = urllib.request.urlopen('https://raw.githubusercontent.com/' + giturl + '/master/MANIFEST.md')
+                            manifest = f.read().decode('utf-8') # this converts it from a byte object to a python string
                         except:
                             manifest = '' # this will cause the fields to be blank
                         indx3 = manifest.find('---') # this bar separates header from "body_text"
                         f2 = StringIO(manifest[:indx3]) # annoying step, pyyaml wants a file object but i already have it in a string, so i wrap the string as a file object
                         md = mistune.Markdown() # at some point they changed the way Markdown objects work
-                        body_text = md.parse(manifest[indx3+3:]) # grab everything after the bar, and run it through markdown parser                    
+                        body_text = md.parse(manifest[indx3+3:]) # grab everything after the bar, and run it through markdown parser
                         #if indx3 != -1:
                         # parse yaml, creates dict, extract what we want
                         try:
-                            processed_yaml = yaml.safe_load(f2) 
+                            processed_yaml = yaml.safe_load(f2)
                             # fetch branches page of github to find the most recent commit
-                            f = urllib.request.urlopen('https://github.com/' + giturl + '/branches') 
-                            branch_page = f.read().decode('utf-8') # this converts it from a byte object to a python string   
+                            f = urllib.request.urlopen('https://github.com/' + giturl + '/branches')
+                            branch_page = f.read().decode('utf-8') # this converts it from a byte object to a python string
                             updateds = [m.start() for m in re.finditer('time-ago datetime=', branch_page)]
                             dates = []
                             for updated in updateds:
                                 date = branch_page[updated+19:updated+29] # pull out date in year-mn-dy format
-                                dates.append(datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))) # parse out year/month/day  
+                                dates.append(datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))) # parse out year/month/day
                             if dates: # if dates is empty its an indication the URL was broken so the OOT doesn't get added to the list
                                 commit_date = max(dates) # most recent commit
                                 if processed_yaml: # if the MANIFEST file existed
                                     new_oots.append(Outoftreemodule(name = giturl.split('/')[1].replace('-','‑'), # people kept giving their stuff long titles, it worked out better to just use their github project url. also, i replace the standard hyphen with a non-line-breaking hyphen =)
-                                                                    tags = ", ".join(processed_yaml.get('tags',['None'])), 
-                                                                    description = processed_yaml.get('brief', 'None'), 
-                                                                    repo = 'https://github.com/' + giturl, # use repo from lwr instead of that provided in manifest 
+                                                                    tags = ", ".join(processed_yaml.get('tags',['None'])),
+                                                                    description = processed_yaml.get('brief', 'None'),
+                                                                    repo = 'https://github.com/' + giturl, # use repo from lwr instead of that provided in manifest
                                                                     last_commit = commit_date,
                                                                     author = ", ".join(processed_yaml.get('author', ['None'])),
                                                                     dependencies = ", ".join(processed_yaml.get('dependencies', ['None'])),
@@ -143,9 +143,9 @@ def refresh(request):
                                                                     body_text = body_text))
                                 else:
                                     new_oots.append(Outoftreemodule(name = giturl.split('/')[1].replace('-','‑'), # people kept giving their stuff long titles, it worked out better to just use their github project url. also, i replace the standard hyphen with a non-line-breaking hyphen =)
-                                                                    tags = 'None', 
-                                                                    description = 'None', 
-                                                                    repo = 'https://github.com/' + giturl, # use repo from lwr instead of that provided in manifest 
+                                                                    tags = 'None',
+                                                                    description = 'None',
+                                                                    repo = 'https://github.com/' + giturl, # use repo from lwr instead of that provided in manifest
                                                                     last_commit = commit_date,
                                                                     author = 'None',
                                                                     dependencies = 'None',
@@ -156,24 +156,24 @@ def refresh(request):
                                                                     body_text = body_text))
                             else:
                                 print('error- recipe ' + recipe + ' had a broken URL')
-                                    
+
                         except yaml.YAMLError:
                             print(giturl, "had error parsing MANIFEST yaml")
                         except urllib.error.HTTPError:
                             print('error opening up the branches page of recipe: ' + recipe)
 
                     else:
-                        print('*** Skipping recipe ' + recipe + ' because its not github based')    
-                else: 
-                    print('error- recipe ' + recipe + ' had no new line at the end of the source field')    
-            else: 
-                print('error- recipe ' + recipe + ' had no source: field')    
-                        
+                        print('*** Skipping recipe ' + recipe + ' because its not github based')
+                else:
+                    print('error- recipe ' + recipe + ' had no new line at the end of the source field')
+            else:
+                print('error- recipe ' + recipe + ' had no source: field')
+
     # Go through and manually add a bunch of OOTs that do not use github
     new_oots.append(Outoftreemodule(name = 'gr-iqbal'.replace('-','‑'), # people kept giving their stuff long titles, it worked out better to just use their github project url. also, i replace the standard hyphen with a non-line-breaking hyphen =)
-                                    tags = ", ".join(['iq imbalance','rx','osmocom']), 
-                                    description = 'GNU Radio block to correct IQ imbalance in quadrature receivers', 
-                                    repo = 'git://git.osmocom.org/gr-iqbal', 
+                                    tags = ", ".join(['iq imbalance','rx','osmocom']),
+                                    description = 'GNU Radio block to correct IQ imbalance in quadrature receivers',
+                                    repo = 'git://git.osmocom.org/gr-iqbal',
                                     last_commit = parse_datetime('2015-11-21 11:47:58'),
                                     author = 'Sylvain Munaut <tnt@246tNt.com>',
                                     dependencies = 'None',
@@ -184,9 +184,9 @@ def refresh(request):
                                     body_text = ' '))
 
     new_oots.append(Outoftreemodule(name = 'gr-fosphor'.replace('-','‑'), # people kept giving their stuff long titles, it worked out better to just use their github project url. also, i replace the standard hyphen with a non-line-breaking hyphen =)
-                                    tags = ", ".join(['fft','gpu','opencl','opengl']), 
-                                    description = 'GNU Radio block for RTSA-like spectrum visualization using OpenCL and OpenGL acceleration', 
-                                    repo = 'git://git.osmocom.org/gr-fosphor', 
+                                    tags = ", ".join(['fft','gpu','opencl','opengl']),
+                                    description = 'GNU Radio block for RTSA-like spectrum visualization using OpenCL and OpenGL acceleration',
+                                    repo = 'git://git.osmocom.org/gr-fosphor',
                                     last_commit = parse_datetime('2018-09-25 11:47:58'),
                                     author = 'Sylvain Munaut <tnt@246tNt.com>',
                                     dependencies = 'None',
@@ -201,7 +201,7 @@ def refresh(request):
     # all the new objects to db
     for new_oot in new_oots:
         new_oot.save() # adds to db
-                                    
+
     table = OutoftreemoduleTable(Outoftreemodule.objects.all(), order_by=("status", "name"))
     RequestConfig(request, paginate={'per_page': 100}).configure(table)
     return render(request, 'ootlist/index.html', {'table': table})
