389ae67e3318122db40de65bc21b71c78a3d3227
Marc <marcll@vt.edu>
2022-12-16 01:34:53 -0500

    fixed up cgran and bumped versions of everything to latest

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 # mostly taken from https://docs.docker.com/compose/django/
-FROM python:3.6
+FROM python:3.10
 ENV PYTHONUNBUFFERED 1  
 RUN mkdir /config  
-ADD requirements.txt /config/  
+ADD requirements.txt /config/
+RUN pip install --upgrade pip
 RUN pip install -r /config/requirements.txt
 RUN mkdir /src;  
 WORKDIR /src
--- ootlist/urls.py
+++ ootlist/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'ootlist'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'submit', views.submit, name='submit'),
-    url(r'refresh', views.refresh, name='refresh'),
-    url(r'^(?P<oot_id>[0-9]+)/$', views.oot_page, name='oot_page'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'submit', views.submit, name='submit'),
+    re_path(r'refresh', views.refresh, name='refresh'),
+    re_path(r'^(?P<oot_id>[0-9]+)/$', views.oot_page, name='oot_page'),
 ]
